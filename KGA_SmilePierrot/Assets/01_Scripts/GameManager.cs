653318f699c64e1afe726730fc03da71c98e4dfc
BBNO <bbno1105@gmail.com>
2022-09-22 16:15:08 +0900

    GameManager 주석 수정
    !!

--- KGA_SmilePierrot/Assets/01_Scripts/GameManager.cs
+++ KGA_SmilePierrot/Assets/01_Scripts/GameManager.cs
@@ -11,7 +11,7 @@ public class GameManager : MonoBehaviour
 
     public bool IsRightColor(int _colorType)
     {
-        if(true) // 추후 플레이어 선택 색상(_colorType)과 CSV 답과 비교하는 코드 추가 필요
+        if(true) // 추후 플레이어 선택 색상(_colorType)과 CSV 답과 비교하는 코드 추가 필요!!
         {
             return true;
         }
